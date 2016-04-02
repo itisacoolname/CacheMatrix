5c0f0b2a6f7ed1b4e76f08e8dd6a93549c568795
Dan Li <348799027@qq.com>
2016-04-03 01:08:11 +0800

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,37 @@
+## This is a imitation function of a cache function for calculating the mean of a numeric
+
+## This function creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- matrix(NA,1,1)
+        set <- function(y) {
+                x <<- y
+                m <<- matrix(NA,1,1)
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. 
+## If the inverse has already been calculated 
+## (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.na(m[1][1])) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+}
